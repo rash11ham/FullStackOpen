ec2d971d8739b6e9ba690a4b545c8888b571f24e
RashedElam <rashedelham3@gmail.com>
2024-08-29 16:08:16 +1000

    exercise-1.4

--- part1/exercise_1.3-1.5/src/App.jsx
+++ part1/exercise_1.3-1.5/src/App.jsx
@@ -2,38 +2,18 @@ const Header = (props) => {
   return <h1>{props.course}</h1>
 }
 
-const Content = () => { 
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+const Content = (props) => { 
+  
   return (
     <div>
-      <Part parts={parts[0].name+" "+parts[0].exercises} />
-      <Part parts={parts[1].name+" "+parts[1].exercises} />
-      <Part parts={parts[2].name+" "+parts[2].exercises} />      
+      <p>{props.parts[0].name} {props.parts[0].exercises}</p>
+      <p>{props.parts[1].name} {props.parts[1].exercises}</p> 
+      <p>{props.parts[2].name} {props.parts[2].exercises}</p>     
     </div>
   )
   
 }
 
-const Part = (props) => {
-  return (
-    <div>
-      <p>{props.parts}</p>
-    </div>
-  )
-}
 
 const Total = (props) => { 
   return <p> Total of the exercises: {
