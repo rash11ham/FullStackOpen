4961bd524d9be850e9179bc4cd23b3466c635573
RashedElam <rashedelham3@gmail.com>
2024-10-04 15:24:58 +1000

    exercise3

--- part3/exercise3/index.js
+++ part3/exercise3/index.js
@@ -1,1 +1,89 @@
-console.log('hello world')
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+
+let notes = [
+  {
+    id: "1",
+    content: "HTML is easy",
+    important: true
+  },
+  {
+    id: "2",
+    content: "Browser can execute only JavaScript",
+    important: false
+  },
+  {
+    id: "3",
+    content: "GET and POST are the most important methods of HTTP protocol",
+    important: true
+  }
+]
+
+app.get('/', (req, res) => {
+    res.send('<h1>Hello world</h1>')
+})
+
+app.get('/api/notes', (req, res) => {
+    res.json(notes)
+})
+
+app.get('/api/notes/:id', (req, res) => {
+    const id = req.params.id
+    const note = notes.find(n => n.id === id)
+    if (!note) {
+        res.status(404).end('No such note found!')
+    } else {
+        res.json(note)
+    }
+    
+})
+
+//POST
+
+function generateId() {
+    const maxId = notes.length > 0
+        ? Math.max(...notes.map(n => Number(n.id)))
+        : 0
+    return String(maxId+1)
+}
+
+app.post('/api/notes', (req, res) => {
+    const body = req.body
+
+    const checkNote = notes.find(n=> body.content === n.content)
+    if (checkNote) {
+        return res.status(409).json({
+            error: "Content already exists"
+        })
+    }
+    if (!body.content) {
+        return res.status(400).json({
+            error: "Content cannot be empty"
+        })
+    }
+
+    const note = {
+        id: generateId(),
+        content: body.content,
+        important: Boolean(body.important) || false
+    }
+
+    notes = notes.concat(note)
+    res.json(note)
+})
+
+//Delete
+app.delete('/api/notes/:id', (req, res) => {
+    const id = req.params.id
+    notes = notes.filter(n => n.id !== id)
+    
+    res.status(204).send('deleted successfuly')
+    
+    
+})
+
+app.listen(3001, () => {
+    console.log('We are connected')
+})
