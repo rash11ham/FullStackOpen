27308efac80c28c1f83dd72390c885f3e1f7871d
RashedElam <rashedelham3@gmail.com>
2024-11-25 19:57:55 +1100

    part5 updated blog exercise 5.7

--- part5/blogs-backend/models/blog.js
+++ part5/blogs-backend/models/blog.js
@@ -5,13 +5,17 @@ const blogSchema = new mongoose.Schema({
     type: String,
     required: true,
     unique: true,
+    
   },
   author: String,
   url: {
     type: String,
     required: true,
   },
-  likes: Number,
+  likes: {
+    type: Number,
+    default: 0
+  },
   user: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "User",
